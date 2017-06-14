c867d66d78595242fa8933a50c4faf835282917a
kennethng2017 <kennethng2017@gmail.com>
2017-06-14 00:28:59 -0400

    Update housingpricemodel.r

--- housingpricemodel.r
+++ housingpricemodel.r
@@ -1,28 +1,31 @@
-run a Linear R on below and send me results. Use 60 Training and 40 Test. Do Random Sampling. 
+#run a Linear R on below and send me results. Use 60 Training and 40 Test. Do Random Sampling.Â 
 
 
-1. With base attributes.
+#1. With base attributes.
 
-2. With added attributes.
+#2. With added attributes.
 
 library(caret)
+setwd("~/data/housingprice")
 
+HometrainX <- read.table("Add_train.csv", header = TRUE, sep = ",")
+HometrainY <- HometrainX$SalePrice
 
+#get 60 train and 43 test data
 set.seed(1)
-
-Rows <- createDataPartition(classes,
-+ p = (100/1459.0),
-+ list= FALSE)
+Rows <- createDataPartition(HometrainY,
+ p = (100/1459.0),
+ list= FALSE)
 
 index <- c(1:60)
 trainset <- Rows[index,]
 
-trainPredictors <- hometrainx[trainset,]
-trainClasses <- hometrainy[trainset]
+trainPredictors <- HometrainX[trainset,]
+trainClasses <- HometrainY[trainset]
 
 
-testPredictors <- hometestx[-trainset,]
-testClasses <- hometesty[-trainset]
+testPredictors <- HometrainX[-trainset,]
+testClasses <- HometrainY[-trainset]
 
 train_data <- trainPredictors
 train_data$SalePrice <- trainClasses
@@ -36,12 +39,16 @@ lmPred1 <- predict(linear_model, testPredictors)
 
 lmValues1 <- data.frame(obs = testClasses, pred = lmPred1)
 
+defaultSummary(lmValues1)
 
-linear_model2 <- lm(SalePrice ~ GarageArea + GrLivArea + TotalBsmtSF + unemplyRate + spOpen + spHigh + spLow + spClose + spAdjClose +spVolume +CCIValue + fedfunds_rate + prime_rate, data = train_data)
+linear_model2 <- lm(SalePrice ~ GarageArea + GrLivArea + TotalBsmtSF + unemplyRate + spOpen + spHigh + spLow + spClose + spAdjClose + spVolume +CCIValue + fedfunds_rate + prime_rate, data = train_data)
 
 
 summary(linear_model2)
 
-lmPred1 <- predict(linear_model2, testPredictors)
+lmPred2 <- predict(linear_model2, testPredictors)
 
-lmValues1 <- data.frame(obs = testClasses, pred = lmPred2)
+lmValues2 <- data.frame(obs = testClasses, pred = lmPred2)
+
+defaultSummary(lmValues2)
+
